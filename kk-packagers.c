c8a87fe4aaec71074f8f84061647f473ddea31e1
tugume-peter <simontugume2@gmail.com>
2022-10-18 13:23:15 +0300

    program ready

--- kk-packagers.c
+++ kk-packagers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -8,22 +9,24 @@
 struct EMPLOYEE_PAY
 {
     char name[50];
-    char position[20];
+    char position[32];
     int working_hours;
     float salary;
 };
 
 void fileRead(FILE *ifp, char *file);
 void fileWrite(FILE *ofp, char *name);
-float getSalary(char *filename, int hours, char *position);
+void addEmployeeDetails(FILE *afp, char *file, struct EMPLOYEE_PAY emp, float dollar);
+float getSalary(char *filename, int hours, char *position, float dollar);
 
 int main(void)
 {
-    FILE *fp;
+    FILE *fp, *wpp;
     char file[] = "weekly_payload.txt";
     char week_id[20];
-    char c, g;
+    char c;
     struct EMPLOYEE_PAY pay;
+    float dollar;
 
     if ((fp = fopen(file, "r")) == NULL)
     {
@@ -57,25 +60,39 @@ int main(void)
 
     printf("\nEnter week identifier: ");
     scanf("%s", week_id);
-    printf("\nEnter employee for the week\n-----------------------------------");
+    printf("Dollar value: ");
+    scanf("%f", &dollar);
+    printf("\nEnter employee details for the week\n-----------------------------------");
     strcat(week_id, ".txt");
 
     fp = fopen(file, "r");
-    do
+
+    if ((wpp = fopen(week_id, "r")) == NULL)
     {
+        fprintf(wpp, "Employee Payment details for week {{%s}}", week_id);
+        fputs("\n------------------------------------------------\n", wpp);
+        fclose(wpp);
+    }
+    else
+    {
+        wpp = fopen(week_id, "a");
+    }
 
-        printf("\nEmployee name: ");
-        scanf("%s", pay.name);
-        printf("Position: ");
-        scanf("%s", pay.position);
-        printf("Working Hours: ");
-        scanf("%d", &pay.working_hours);
+    addEmployeeDetails(wpp, file, pay, dollar);
+    fileRead(wpp, week_id);
 
-        pay.salary = getSalary(file, pay.working_hours, pay.position);
+    printf("\n\nAdd other employee pay details? -- y/n: ");
+    scanf(" %c", &c);
 
-        printf("Add employee? -- y/n: ");
-        scanf(" %c", &g);
-    } while ((g == 'y' || g == 'Y'));
+    if (c == 'y' || c == 'Y')
+    {
+        addEmployeeDetails(wpp, file, pay, dollar);
+    }
+    else
+    {
+        fclose(wpp);
+        return (1);
+    }
 
     return (0);
 }
@@ -117,21 +134,94 @@ void fileWrite(FILE *ofp, char *name)
     fclose(ofp);
 }
 
-float getSalary(char *filename, int hours, char *position)
+float getSalary(char *filename, int hours, char *position, float dollarValue)
 {
     FILE *rfp;
     float payable;
+    int l = strlen(position);
+    int i, j;
+    char c;
+    char qstr[16], tmp[16], string[100], next1[5], next2[5];
+    float h_rate, h_o_rate;
 
     if ((rfp = fopen(filename, "r")) == NULL)
     {
         printf("Error reading file!");
-        return (0);
+        return (-1);
     }
 
     if (hours < 40)
     {
         payable = 0.0;
     }
+    else if (hours >= 40)
+    {
+        if (strstr(position, " "))
+        {
+            i = l - 1;
+            j = 0;
+            while (position[i] != ' ')
+            {
+                c = position[i];
+                tmp[j] = c;
+                j++;
+                i--;
+            }
+            j = strlen(tmp) - 1;
+            i = 0;
+            while (j > -1)
+            {
+                qstr[i] = tmp[j];
+                i++;
+                j--;
+            }
+        }
+        else
+        {
+            i = 0;
+            while (i < l)
+            {
+                qstr[i] = position[i];
+            }
+        }
+
+        while ((fscanf(rfp, "%s", string)) == 1)
+        {
+            if (strstr(string, qstr))
+            {
+                fscanf(rfp, "%s", next1);
+                fscanf(rfp, "%s", next2);
+            }
+        }
+
+        h_rate = atof(next1);
+        h_o_rate = atof(next2);
+
+        payable = dollarValue * (40 * h_rate + (hours - 40) * h_o_rate);
+    }
+
+    fclose(rfp);
 
     return payable;
+}
+
+void addEmployeeDetails(FILE *afp, char *file, struct EMPLOYEE_PAY emp, float dollar)
+{
+    char g;
+    do
+    {
+        printf("\nEmployee name: ");
+        scanf(" %[^\n]%*c", emp.name);
+        printf("Position: ");
+        scanf(" %[^\n]%*c", emp.position);
+        printf("Working Hours: ");
+        scanf("%d", &emp.working_hours);
+
+        emp.salary = getSalary(file, emp.working_hours, emp.position, dollar);
+        fprintf(afp, "%s\t\t%s\t\t%d\t%0.2f\n", emp.name, emp.position, emp.working_hours, emp.salary);
+
+        printf("Add employee? -- y/n: ");
+        scanf(" %c", &g);
+    } while ((g == 'y' || g == 'Y'));
+    fclose(afp);
 }
